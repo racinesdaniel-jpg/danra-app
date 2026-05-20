a2125e35adef925247b824e6b71ede807cb42a32
Your Name <you@example.com>
2026-05-19 22:11:15 -0500

    Implementacion Firebase - Loguin v4

--- src/Login.jsx
+++ src/Login.jsx
@@ -31,23 +31,22 @@ export default function Login({ onLogin }) {
   };
 
   return (
-    <div className="min-h-screen bg-black flex items-center justify-center p-6">
+  <div className="min-h-screen bg-black flex items-center justify-center p-6">
+    <form
+      onSubmit={handleLogin}
+      className="bg-zinc-900 p-10 rounded-3xl border border-zinc-800 w-full max-w-md"
+    >
+      <h1 className="text-5xl font-bold text-white mb-10">
+        Acceso DANRA
+      </h1>
 
-      <form
-        onSubmit={handleLogin}
-        className="bg-zinc-900 p-10 rounded-3xl border border-zinc-800 w-full max-w-md"
-      >
-
-        <h1 className="text-3xl text-white font-bold mb-8">
-          Acceso DANRA
-        </h1>
-
+      <div className="space-y-5">
         <input
           type="email"
           placeholder="Correo"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          className="w-full mb-4 p-4 rounded-xl bg-zinc-950 border border-zinc-700 text-white"
+          className="w-full bg-black border border-zinc-700 rounded-2xl px-5 py-4 text-white"
         />
 
         <input
@@ -55,28 +54,29 @@ export default function Login({ onLogin }) {
           placeholder="Contraseña"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          className="w-full mb-6 p-4 rounded-xl bg-zinc-950 border border-zinc-700 text-white"
+          className="w-full bg-black border border-zinc-700 rounded-2xl px-5 py-4 text-white"
         />
 
         {error && (
-          <p className="text-red-500 mb-4">
+          <p className="text-red-400 text-sm">
             {error}
           </p>
         )}
 
         <button
           type="submit"
-          className="w-full bg-amber-500 hover:bg-amber-400 text-black font-bold py-4 rounded-xl"
+          className="w-full bg-amber-500 hover:bg-amber-400 text-black font-bold py-4 rounded-2xl"
         >
           Ingresar
         </button>
-<p
-  onClick={handleRegister}
-  className="text-center text-sm text-zinc-400 mt-4 cursor-pointer hover:text-amber-400 transition-all"
->
-  ¿No tienes cuenta? Crear cuenta
-</p>
-      </form>
-    </div>
-  );
-}
+
+        <p
+          onClick={handleRegister}
+          className="text-center text-sm text-zinc-400 mt-4 cursor-pointer hover:text-amber-400 transition-all"
+        >
+          ¿No tienes cuenta? Crear cuenta
+        </p>
+      </div>
+    </form>
+  </div>
+);
